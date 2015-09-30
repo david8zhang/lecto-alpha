b75ef51353fcc9ecb03ccfc9d0793c83ab96e4cc
David Zhang <david8zhang@berkeley.edu>
2015-09-29 23:21:15 -0700

    fixed heroku bugs

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var express = require('express');
 var flash = require('connect-flash');
 var app = express();
 // var ExpressPeerServer = require('peer').ExpressPeerServer;
-var port = process.env.PORT || 5400;
-var server = require('http').createServer(app).listen(port);
+var server = require('http').createServer(app).listen(5400);
 console.log("server listening on 5400")
 
 //Initialize Socket.io
